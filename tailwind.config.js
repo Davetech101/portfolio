51fcce9ee02f5658a82e97d66952cd15355ffb21
Ogaranya <enomahog@gmail.com>
2023-03-02 09:24:33 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {      colors: {
+      'blue': '#243c5a',
+      'red': '#FF0000',
+    }},
+  },
+  plugins: [],
+}
