3f22e71d831a93bc4d4e4e74b6ff0b6e3ddb44b5
Ogaranya <enomahog@gmail.com>
2023-03-03 11:39:12 +0100

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,36 +1,49 @@
-import Link from "next/link"
-import { HiLightBulb } from 'react-icons/hi';
+import Link from "next/link";
+import { HiLightBulb } from "react-icons/hi";
 
 const Navbar = () => {
+  const navLinks = [
+    {
+      title: "Portfolio",
+      href: "portfolio",
+    },
+    {
+      title: "About Me",
+      href: "about",
+    },
+    {
+      title: "Contact me",
+      href: "Contact",
+    },
+  ];
+
+  const nav = navLinks.map((link) => (
+    <li key={link.href}>
+      <Link className="text-3xl dark:text-white-2" href={link.href}>{link.title}</Link>
+    </li>
+  ));
+
   return (
     <>
-        <header className="dark:text-white flex items-center justify-between">
-            <Link href="/">
-                <h2 className="text-3xl font-bold underline text-red dark:text-blue">David Enomah</h2>
-            </Link>
+      <header className="dark:text-white flex items-center justify-between">
+        <Link href="/">
+          <h2 className="text-6xl font-bold dark:text-white">David Enomah</h2>
+        </Link>
 
-            <div className="flex items-center justify-between">
-                <nav >
-                <ul className="flex items-center justify-between">
-                    <li>
-                        <Link href="/portfolio">Portfolio</Link>
-                    </li>
-                    <li>
-                        <Link href="about">About Me</Link>
-                    </li>
-                    <li>
-                        <Link href="contact">Contact me</Link>
-                    </li>
-                </ul>
-            </nav>
+        <div className="flex items-center justify-between w-2/5 gap-20">
+          <nav className="w-full">
+            <ul className="flex items-center justify-between">
+              {nav}
+            </ul>
+          </nav>
 
-            <button>
-                <HiLightBulb/>
-            </button>
-            </div>
-        </header>
+          <button className="text-6xl rotate-180">
+            <HiLightBulb />
+          </button>
+        </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
