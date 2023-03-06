412d95031e9fce8ff8da0e6d00468263b0f1eed7
Ogaranya <enomahog@gmail.com>
2023-03-06 23:01:28 +0100

    connect

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -7,27 +7,35 @@ import { TbFileDownload } from "react-icons/tb";
 const Connect = () => {
   const media = [
     {
+      name: "LinkedIn",
       href: "",
-      svg: <GrLinkedinOption/>
-    },{
+      svg: <GrLinkedinOption />,
+    },
+    {
+      name: "Github",
       href: "",
-      svg: <BsGithub/>
-    },{
+      svg: <BsGithub />,
+    },
+    {
+      name: "Twitter",
       href: "",
-      svg: <AiOutlineTwitter/>
-    },{
+      svg: <AiOutlineTwitter />,
+    },
+    {
+      name: "Download Resume",
       href: "",
-      svg: <TbFileDownload/>
-    }
-  ]
-  const yes = "TbFileDownload"
+      svg: <TbFileDownload />,
+    },
+  ];
+  const yes = "TbFileDownload";
   return (
     <div className="absolute bottom-10 right-10 flex flex-col gap-10">
-      {
-        media.map((medium, idx) => (
-          <Link key={idx} href={medium.href} className="text-white">{medium.svg}</Link>
-        ))
-      }
+      {media.map((medium, idx) => (
+        <Link key={idx} href={medium.href} className="text-white text-4xl">
+          <span>{medium.name}</span>
+          <span>{medium.svg}</span>{" "}
+        </Link>
+      ))}
     </div>
   );
 };
