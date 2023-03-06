e10a77cb775239357d3ed7272c09c83e99e378be
Ogaranya <enomahog@gmail.com>
2023-03-06 22:45:09 +0100

    layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 const Layout = () => {
   return (
     <div className='p-10 rounded-2xl h-screen'>
-      <div className="border-2 border-white-2 border-solid p-10 rounded-2xl min-h-full">
+      <div className="border-2 border-white-2 border-solid p-10 rounded-2xl min-h-full relative">
       <Navbar/> 
       <Connect/>
       </div>
