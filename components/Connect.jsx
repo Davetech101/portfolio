47c080e65243f00c11c0d9ec647f342d7b1c9482
Ogaranya <enomahog@gmail.com>
2023-03-06 22:44:55 +0100

    connect

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -5,7 +5,7 @@ import { TbFileDownload } from "react-icons/tb";
 
 const Connect = () => {
   return (
-    <div>
+    <div className="absolute bottom-10 right-10 flex flex-col gap-10">
       <GrLinkedinOption color="white" />
       <BsGithub color="white" />
       <AiOutlineTwitter color="white" />
