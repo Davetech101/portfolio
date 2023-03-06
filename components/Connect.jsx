4cc19cc04e7b35e485310de0230de6270230da37
Ogaranya <enomahog@gmail.com>
2023-03-06 23:25:59 +0100

    connect

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -3,8 +3,11 @@ import { GrLinkedinOption } from "react-icons/gr";
 import { BsGithub } from "react-icons/bs";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { TbFileDownload } from "react-icons/tb";
+import { useState } from "react";
 
 const Connect = () => {
+  const [hover, setHover] = useState(null);
+
   const media = [
     {
       name: "LinkedIn",
@@ -29,10 +32,22 @@ const Connect = () => {
   ];
   const yes = "TbFileDownload";
   return (
-    <div className="absolute bottom-10 right-10 flex flex-col gap-10">
+    <div className="absolute bottom-10 right-10 flex flex-col gap-10 items-end">
       {media.map((medium, idx) => (
-        <Link key={idx} href={medium.href} className="text-white text-4xl">
-          <span>{medium.name}</span>
+        <Link
+          key={idx}
+          href={medium.href}
+          className="text-white text-4xl flex items-center gap-4"
+          onMouseEnter={() => setHover(idx)}
+          onMouseLeave={() => setHover(null)}
+        >
+          <span
+            className={`text-2xl text-white-2 ${
+              hover === idx ? "translate-x-0" : "translate-x-40"
+            } transition-all`}
+          >
+            {medium.name}
+          </span>
           <span>{medium.svg}</span>{" "}
         </Link>
       ))}
@@ -41,3 +56,4 @@ const Connect = () => {
 };
 
 export default Connect;
+0;
