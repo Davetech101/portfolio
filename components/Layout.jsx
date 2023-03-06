1e271b300233aa868139da8c494275026c57c218
Ogaranya <enomahog@gmail.com>
2023-03-06 23:41:41 +0100

    layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,12 @@
 import Connect from './Connect'
 import Navbar from './Navbar'
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <div className='p-10 rounded-2xl h-screen'>
       <div className="border-2 border-white-2 border-solid p-10 rounded-2xl min-h-full relative">
       <Navbar/> 
+      {children}
       <Connect/>
       </div>
     </div>
