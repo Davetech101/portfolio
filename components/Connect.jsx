9f2a451f33e45d40e2a9c4031de94da770785173
Ogaranya <enomahog@gmail.com>
2023-03-06 22:55:29 +0100

    connect

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -1,15 +1,33 @@
+import Link from "next/link";
 import { GrLinkedinOption } from "react-icons/gr";
 import { BsGithub } from "react-icons/bs";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { TbFileDownload } from "react-icons/tb";
 
 const Connect = () => {
+  const media = [
+    {
+      href: "",
+      svg: <GrLinkedinOption/>
+    },{
+      href: "",
+      svg: <BsGithub/>
+    },{
+      href: "",
+      svg: <AiOutlineTwitter/>
+    },{
+      href: "",
+      svg: <TbFileDownload/>
+    }
+  ]
+  const yes = "TbFileDownload"
   return (
     <div className="absolute bottom-10 right-10 flex flex-col gap-10">
-      <GrLinkedinOption color="white" />
-      <BsGithub color="white" />
-      <AiOutlineTwitter color="white" />
-      <TbFileDownload color="white"/>
+      {
+        media.map((medium, idx) => (
+          <Link key={idx} href={medium.href} className="text-white">{medium.svg}</Link>
+        ))
+      }
     </div>
   );
 };
