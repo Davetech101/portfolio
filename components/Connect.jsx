188405f4e8526223aa2a0e9c9eec29fbee0c51c8
Ogaranya <enomahog@gmail.com>
2023-03-06 22:34:07 +0100

    connect

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -1,7 +1,17 @@
+import { GrLinkedinOption } from "react-icons/gr";
+import { BsGithub } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
+
+
+
+
 const Connect = () => {
   return (
     <div>
-        
+      <GrLinkedinOption color="white"/>
+      <BsGithub color="white"/>
+      <AiOutlineTwitter color="white"/>
+      
     </div>
   )
 }
