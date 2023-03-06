351e3bc07dad8b6c8854c6b523d955257461bf15
Ogaranya <enomahog@gmail.com>
2023-03-06 22:39:18 +0100

    connect

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -1,19 +1,17 @@
 import { GrLinkedinOption } from "react-icons/gr";
 import { BsGithub } from "react-icons/bs";
 import { AiOutlineTwitter } from "react-icons/ai";
-
-
-
+import { TbFileDownload } from "react-icons/tb";
 
 const Connect = () => {
   return (
     <div>
-      <GrLinkedinOption color="white"/>
-      <BsGithub color="white"/>
-      <AiOutlineTwitter color="white"/>
-      
+      <GrLinkedinOption color="white" />
+      <BsGithub color="white" />
+      <AiOutlineTwitter color="white" />
+      <TbFileDownload color="white"/>
     </div>
-  )
-}
+  );
+};
 
-export default Connect
+export default Connect;
