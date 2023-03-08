2c691320bcf948eb957251cc338646d41610078a
Ogaranya <enomahog@gmail.com>
2023-03-08 12:12:54 +0100

    about

---
+++ components/AboutMe.jsx
@@ -0,0 +1,15 @@
+import P from "./P";
+
+const About = () => {
+  return (
+    <div className="absolute top-2/4 -translate-y-1/2 w-9/12">
+      <P>
+        Lorem ipsum dolor sit amet consecteur adipscing Lorem ipsum dolor sit
+        amet consecteur adipscing Lorem ipsum dolor sit amet consecteur
+        adipscing Lorem ipsum dolor sit amet consecteur adipscing
+      </P>
+    </div>
+  );
+};
+
+export default About;
