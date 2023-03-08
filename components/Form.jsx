f8a2cda527022fcbc0e8d0f3cd93a4d94e1b7e25
Ogaranya <enomahog@gmail.com>
2023-03-08 08:37:31 +0100

    form

---
+++ components/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='absolute top-2/4 -translate-y-3/4'>
+        <p className="">
+            Have a message?
+        </p>
+
+        <form action="" className="">
+            <input type="text" className="border-2 border-white-2 border-solid" /> <input type="email" className="border-2 border-white-2 border-solid" />
+            <textarea name="" id="" cols="30" rows="10" className="border-2 border-white-2 border-solid"></textarea>
+        </form>
+    </div>
+  )
+}
+
+export default Form
