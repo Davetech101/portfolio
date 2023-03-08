220e74e8f0be7f41c91be75c5a29f587c0cb5c4a
Ogaranya <enomahog@gmail.com>
2023-03-08 09:03:55 +0100

    form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import { GrLinkedinOption } from "react-icons/gr";
+import { BsGithub } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
 
 const Form = () => {
+    const inputStyles = "border-2 border-white-2 border-solid rounded-xl p-5 w-full text-2xl"
   return (
-    <div className='absolute top-2/4 -translate-y-3/4'>
+    <div className="absolute top-2/4 -translate-y-3/4 w-9/12">
+      <p className="text-5xl mb-6">Have a message?</p>
+
+      <form action="" className="flex justify-between w-full gap-10">
+       <div className="flex flex-col gap-10 w-full">
+       <input type="text" className={inputStyles} placeholder="Name"/>{" "}
+        <input type="email" className={inputStyles} placeholder="Email"/>
+       </div>
+        <textarea
+          name=""
+          id=""
+          className={inputStyles}
+          placeholder="message"
+        ></textarea>
+      </form>
+
+      <div className="">
         <p className="">
-            Have a message?
+            Let&apos;s 
+            <span className="">Connect</span>
         </p>
 
-        <form action="" className="">
-            <input type="text" className="border-2 border-white-2 border-solid" /> <input type="email" className="border-2 border-white-2 border-solid" />
-            <textarea name="" id="" cols="30" rows="10" className="border-2 border-white-2 border-solid"></textarea>
-        </form>
+        <div className="">
+        <GrLinkedinOption /> <BsGithub /> <AiOutlineTwitter />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
