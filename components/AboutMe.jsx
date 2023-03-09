2ae8a0debde49c84744efcaf5e8b323c9607ce78
Ogaranya <enomahog@gmail.com>
2023-03-09 18:04:07 +0100

    about me

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,13 +1,22 @@
+import Cube from "./Cube";
 import P from "./P";
 
 const About = () => {
   return (
-    <div className="absolute top-2/4 -translate-y-1/2 w-9/12">
-      <P>
+    <div className="absolute top-2/4 -translate-y-1/2 w-8/12 flex items-center justify-between">
+     <div className="max-w-4xl">
+     <P>
         Lorem ipsum dolor sit amet consecteur adipscing Lorem ipsum dolor sit
         amet consecteur adipscing Lorem ipsum dolor sit amet consecteur
         adipscing Lorem ipsum dolor sit amet consecteur adipscing
       </P>
+
+      <button className="text-4xl mt-5">
+        Resume
+      </button>
+     </div>
+
+      <Cube/>
     </div>
   );
 };
