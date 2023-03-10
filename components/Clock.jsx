db63affbabc1ba17e2496e8a585217a6e3022179
Ogaranya <enomahog@gmail.com>
2023-03-10 21:13:08 +0100

    clock

---
+++ components/Clock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Clock = () => {
+  return (
+    <div className="clock-body ">
+      <div className="analogue-clock"></div>
+    </div>
+  );
+};
+
+export default Clock;
