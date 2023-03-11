1e939161930aee1ac856a46c0793f68a109a85c2
Ogaranya <enomahog@gmail.com>
2023-03-11 17:29:23 +0100

    layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 
 const Layout = ({children}) => {
   return (
-    <div className='p-10 rounded-2xl h-screen'>
+    <div className='p-10 rounded-2xl h-screen flex flex-col justify-center'>
       <div className="border-2 border-white-2 border-solid p-10 rounded-2xl min-h-full relative">
       <Navbar/> 
       {children}
