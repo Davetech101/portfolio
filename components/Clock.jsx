06b64a51bce38d0427cb17287286bf0657ff28cc
Ogaranya <enomahog@gmail.com>
2023-03-11 17:51:49 +0100

    clock

--- components/Clock.jsx
+++ components/Clock.jsx
@@ -22,7 +22,8 @@ export default class Clock extends Component {
 
   render() {
     return (
-      <div className="clock">
+      <div className="container">
+        <div className="clock">
         <div
           className="hour_hand"
           style={{
@@ -41,18 +42,19 @@ export default class Clock extends Component {
             transform: `rotateZ(${this.state.time.getSeconds() * 6}deg)`
           }}
         />
-        <span className="twelve">12</span>
+        <span className="twelve underline rotate-90">Twelve</span>
         <span className="one">1</span>
         <span className="two">2</span>
-        <span className="three">3</span>
+        <span className="three underline">Three</span>
         <span className="four">4</span>
         <span className="five">5</span>
-        <span className="six">6</span>
+        <span className="six underline rotate-90">Six</span>
         <span className="seven">7</span>
         <span className="eight">8</span>
-        <span className="nine">9</span>
+        <span className="nine underline">Nine</span>
         <span className="ten">10</span>
         <span className="eleven">11</span>
+      </div>
       </div>
     );
   }
