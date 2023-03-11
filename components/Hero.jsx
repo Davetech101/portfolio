864bc0c42b5a5c8dcdf7b39594b366daf89f10fb
Ogaranya <enomahog@gmail.com>
2023-03-11 17:52:02 +0100

    hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ import P from "./P";
 const Hero = () => {
   return (
     <div className="flex absolute top-2/4 -translate-y-2/4 p-40 flex justify-between items-center w-full">
-      <main className="relative">
+      <main className="relative w-6/12">
         <div className="absolute w-0.5 h-32 bg-purple mb-5 -top-16 -left-7"></div>
         <div className="absolute h-0.5 w-32 bg-purple mb-5 -top-7 -left-16"></div>
         <p className="uppercase text-3xl underline">
