a07f33b21b6943175dd8d1bd32e0e6db559896ec
Ogaranya <enomahog@gmail.com>
2023-03-13 07:41:40 +0100

    about

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
-import AboutMe from '@/components/AboutMe'
+import AboutMe from "@/components/AboutMe";
+import Navbar from "@/components/Navbar";
 
 export default function About() {
-    return (
-      <>
-        <AboutMe/>
-      </>
-    )
+  return (
+    <section className="overflow-hidden h-screen">
+      <Navbar />
+      <AboutMe/>
+    </section>
+  );
 }
-  
