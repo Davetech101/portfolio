f3f9186369f19a843c423bffa272cea6428a5cb3
Ogaranya <enomahog@gmail.com>
2023-03-13 07:40:54 +0100

    about me

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,23 +1,39 @@
-import Cube from "./Cube";
-import P from "./P";
+import Link from "next/link";
+import Typewriter from "typewriter-effect";
+import StyledAboutMe from "@/styles/styledcomponents/StyledAboutMe";
 
 const About = () => {
   return (
-    <div className="absolute top-2/4 -translate-y-1/2 w-8/12 flex items-center justify-between">
-     <div className="max-w-4xl">
-     <P>
-        Lorem ipsum dolor sit amet consecteur adipscing Lorem ipsum dolor sit
-        amet consecteur adipscing Lorem ipsum dolor sit amet consecteur
-        adipscing Lorem ipsum dolor sit amet consecteur adipscing
-      </P>
+    <StyledAboutMe>
+      <span className="po flex">{"<h2>"}</span>
+      <h2>
+        <Typewriter
+          className="text-orange hidden"
+          onInit={(typewriter) => {
+            typewriter
+            .typeString("Enomah")
+            .deleteAll()
+            .typeString("David Ogaranya Enomah")
+            .start();
+          }}
+        />
+      </h2>
+      <span className="pe -mt-5">{"</h2>"}</span>
 
-      <button className="text-4xl mt-5">
-        Resume
-      </button>
-     </div>
-
-      <Cube/>
-    </div>
+      <span className="pe block">{"</p>"}</span>
+      <p>
+        I am a Frontend developer located in Nigeria. Ardent towards UI effects,
+        animation and creating intuitive, dynamic user experiences. <br /> A
+        well-organised, problem solving and independent indivdual who pays high
+        attention to details. A Football fan, chess player and also a lover of
+        poetry. <br /> Currently undergoing my BSc in Electronics and Computer
+        Engineering at the University of Port Harcourt, Nigeria! <br />
+        Interested in the entire Web development spectrum and working on
+        mind-blowing projects with positive developers!
+        <Link href="/contact">Let&apos;s make something special</Link>
+      </p>
+      <span className="pe">{"</p>"}</span>
+    </StyledAboutMe>
   );
 };
 
