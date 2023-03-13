0cd7729d9945432abb95846f12beaf70cfac680f
Ogaranya <enomahog@gmail.com>
2023-03-13 07:42:20 +0100

    styled components

---
+++ styles/styledcomponents/StyledAboutMe.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const StyledAboutMe = styled.section`
+  margin-top: 4rem;
+  padding: 4rem;
+  position: relative;
+
+  h2 {
+    font-family: "Permanent Marker", cursive;
+    color: #f19c00;
+    font-size: 5rem;
+    display: inline;
+  }
+
+  h2 div {
+    margin-left: 5rem;
+  }
+
+  .po,
+  .pe {
+    font-family: "Permanent Marker", cursive;
+    color: #f19c0036;
+    font-size: 2rem;
+  }
+
+  p {
+    font-size: 2rem;
+    font-family: "Sono", sans-serif;
+    margin-left: 5rem;
+  }
+`;
+
+export default StyledAboutMe;
