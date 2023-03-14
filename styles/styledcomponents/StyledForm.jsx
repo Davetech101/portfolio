effb7c8577da4122c220f0d9cd77cf5a781d1244
Ogaranya <enomahog@gmail.com>
2023-03-14 13:53:01 +0100

    styled components

--- styles/styledcomponents/StyledForm.jsx
+++ styles/styledcomponents/StyledForm.jsx
@@ -31,7 +31,7 @@ const StyledForm = styled.section`
     flex-direction:column;
     max-width:50rem;
     gap:1rem;
-    margin-left:5rem;
+    margin:2rem 5rem;
   }
 
   .input{
@@ -41,16 +41,54 @@ const StyledForm = styled.section`
     font-size:2rem;
     font-family: 'Indie Flower', cursive;
     outline:none;
+    // border-bottom:1px dashed #f19c00;
+    caret-color:#f19c00;
+    transition:all .4s;
   }
 
+  .input:focus{
+  }
+
+  .input1:focus{
+    box-shadow:1px 1px 1px #f19c0036 ;
+    transform:translateY(-1rem);
+  }
+  
+
   textarea{
-    min-height:20rem;
+    min-height:5rem;
+    transition:all .4s;
+  }
+
+  textarea:focus{
+    min-height:15rem;
   }
 
   button{
     padding:1rem 2rem;
     font-size:2rem;
     font-family: 'Indie Flower', cursive;
+    border:1px solid #f19c00;
+    // max-width:20rem;
+    position:relative;
+    overflow:hidden;
+  }
+
+  button::before{
+    content:"";
+    position:absolute;
+    top:0;
+    left:0;
+    z-index:-1;
+    width:0%;
+    height:100%;
+    background:#f19c00;
+    border-top-right-radius:7rem;
+    transition:all .5s;
+  }
+
+  button:hover::before{
+    width:130%;
   }
 `;
 
