1dc7f70772428c9201dbc2d279100cf0461dfe40
Ogaranya <enomahog@gmail.com>
2023-03-14 19:23:00 +0100

    form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,64 +1,78 @@
-import Link from "next/link";
-import { GrLinkedinOption } from "react-icons/gr";
 import { GiPawn } from "react-icons/gi";
-import { AiOutlineTwitter } from "react-icons/ai";
+import { GiRingingBell } from "react-icons/gi";
 import StyledForm from "@/styles/styledcomponents/StyledForm";
 import Typewriter from "typewriter-effect";
+import { useState } from "react";
 
 const Form = () => {
+  const [ring, setRing] = useState(false);
+
   return (
     <StyledForm>
-      <GiPawn className="pawn"/>
-      <span className="po">{"<p>"}</span>
-      <h2 className="">
-        Have A
-        <Typewriter
-          className="text-orange hidden"
-          onInit={(typewriter) => {
-            typewriter
-              .typeString("Question?")
-              .deleteAll()
-              .typeString("Suggestion?")
-              .deleteAll()
-              .typeString("Nice day!")
-              .pauseFor(3500)
-              .deleteAll()
-              .typeString("Question?")
-              .deleteAll()
-              .typeString("Suggestion?")
-              .deleteAll()
-              .typeString("Nice day!")
-              .pauseFor(3500)
-              .deleteAll()
-              .typeString("Question?")
-              .deleteAll()
-              .typeString("Suggestion?")
-              .deleteAll()
-              .typeString("Nice day!")
-              .pauseFor(3500)
-              .deleteAll()
-              .typeString("Question?")
-              .deleteAll()
-              .typeString("Suggestion?")
-              .deleteAll()
-              .typeString("Nice day!")
-              .start();
-          }}
-        />
-      </h2>
-      <span className="pe block">{"</p>"}</span>
-      <span className="po">{"<form>"}</span>
-      <form action="" className="">
-        <div className="flex items-center gap-3">
-          <input type="text" className="input input1" placeholder="Name" />{" "}
-          <input type="email" className="input input1" placeholder="Email" />
-        </div>
-        <input type="text" placeholder="Subject" className="input"/>
-        <textarea name="" id="" className="input" placeholder="message"></textarea>
-        <button>Send!</button>
-      </form>
-      <span className="po">{"</form>"}</span>
-
+      <GiPawn className="pawn" />
+      <GiRingingBell
+        className={ring ? "bell ringbell" : "bell"}
+        onMouseEnter={() => setRing(true)}
+        onMouseLeave={() => setRing(false)}
+      />
+      <div className="part1">
+        <span className="po">{"<h2>"}</span>
+        <h2 className="">
+          Have A
+          <Typewriter
+            className="text-orange hidden"
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Question?")
+                .deleteAll()
+                .typeString("Suggestion?")
+                .deleteAll()
+                .typeString("Nice day!")
+                .pauseFor(3500)
+                .deleteAll()
+                .typeString("Question?")
+                .deleteAll()
+                .typeString("Suggestion?")
+                .deleteAll()
+                .typeString("Nice day!")
+                .pauseFor(3500)
+                .deleteAll()
+                .typeString("Question?")
+                .deleteAll()
+                .typeString("Suggestion?")
+                .deleteAll()
+                .typeString("Nice day!")
+                .pauseFor(3500)
+                .deleteAll()
+                .typeString("Question?")
+                .deleteAll()
+                .typeString("Suggestion?")
+                .deleteAll()
+                .typeString("Nice day!")
+                .start();
+            }}
+          />
+        </h2>
+        <span className="pe block">{"</h2>"}</span>
+      </div>
+      <div className="part2">
+        <span className="po">{"<form>"}</span>
+        <form action="" className="">
+          <div className="flex items-center gap-3">
+            <input type="text" className="input input1" placeholder="Name" />{" "}
+            <input type="email" className="input input1" placeholder="Email" />
+          </div>
+          <input type="text" placeholder="Subject" className="input" />
+          <textarea
+            name=""
+            id=""
+            className="input"
+            placeholder="message"
+          ></textarea>
+          <button>Send!</button>
+        </form>
+        <span className="po">{"</form>"}</span>
+      </div>
     </StyledForm>
   );
 };
