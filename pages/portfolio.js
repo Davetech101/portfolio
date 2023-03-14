0bd0ecd92dc54deea73c8a425aa80cb3b3dcb2b6
Ogaranya <enomahog@gmail.com>
2023-03-14 19:23:11 +0100

    portfolio

---
+++ pages/portfolio.js
@@ -0,0 +1,10 @@
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+
+export default function Portfolio() {
+  return (
+    <section className="">
+    <Navbar/>
+    </section>
+  )
+}
