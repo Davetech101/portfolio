7b4a60ab62ec92313d8ec9bae19bef8e104533d8
Ogaranya <enomahog@gmail.com>
2023-03-16 14:18:08 +0100

    projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,6 +2,63 @@ import StyledProjects from "@/styles/styledcomponents/StyledProjects";
 import Typewriter from "typewriter-effect";
 
 const Projects = () => {
+  const projects = [
+    {
+      id: "",
+      name: "",
+      tech: "",
+      about: "",
+      mobile: "",
+      desktop: "",
+      tablet: "",
+      github: "",
+      link: "",
+    },
+    {
+      id: "",
+      name: "",
+      tech: "",
+      about: "",
+      mobile: "",
+      desktop: "",
+      tablet: "",
+      github: "",
+      link: "",
+    },
+    {
+      id: "",
+      name: "",
+      tech: "",
+      about: "",
+      mobile: "",
+      desktop: "",
+      tablet: "",
+      github: "",
+      link: "",
+    },
+    {
+      id: "",
+      name: "",
+      tech: "",
+      about: "",
+      mobile: "",
+      desktop: "",
+      tablet: "",
+      github: "",
+      link: "",
+    },
+    {
+      id: "",
+      name: "",
+      tech: "",
+      about: "",
+      mobile: "",
+      desktop: "",
+      tablet: "",
+      github: "",
+      link: "",
+    },
+  ];
   return (
     <StyledProjects>
       <span className="po">{"<h2>"}</span>
