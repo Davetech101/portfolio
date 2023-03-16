5e2bdf49eee84f2d72f439dabb465d99579029e9
Ogaranya <enomahog@gmail.com>
2023-03-16 14:08:24 +0100

    portfolio

--- styles/styledcomponents/StyledProjects.jsx
+++ styles/styledcomponents/StyledProjects.jsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
 const StyledProjects = styled.div`
+  margin-top: 2rem;
+  padding: 4rem;
+  position: relative;
   
+  .po,
+  .pe {
+    font-family: "Permanent Marker", cursive;
+    color: #f19c0036;
+    font-size: 2rem;
+  }
 `;
 
 export default StyledProjects;
