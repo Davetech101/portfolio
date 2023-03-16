50d5d4050e6821ddcf8f8e68ca6b8947b8bc9d4f
Ogaranya <enomahog@gmail.com>
2023-03-16 14:02:59 +0100

    projects

---
+++ components/Projects.jsx
@@ -0,0 +1,12 @@
+import StyledProjects from '@/styles/styledcomponents/StyledProjects'
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <StyledProjects>
+        
+    </StyledProjects>
+  )
+}
+
+export default Projects
