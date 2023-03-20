78b1411467791b84fdaa98297ebca2199679b4a5
Ogaranya <enomahog@gmail.com>
2023-03-20 14:33:18 +0100

    projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -12,9 +12,9 @@ const Projects = () => {
       tech: "Next js, Typescript",
       about:
         "Book lodgings, go sight seeing, engage in fun activities. Coventry hotel aids tourists get accomodation and amazing tourism experiences.",
-      mobile: "/assets/testMob.jpeg",
-      desktop: "/assets/testTab.jpeg",
-      tablet: "/assets/testDex.jpeg",
+      mobile: "/assets/covM.jpeg",
+      desktop: "/assets/covT.jpeg",
+      tablet: "/assets/covD.jpeg",
       github: "https://github.com/Davetech101/coventry-hotel",
       link: "",
     },
@@ -23,12 +23,12 @@ const Projects = () => {
       name: "rest countries",
       tech: "React js, Javascript",
       about:
-        "An app that displays all the countries in the world and important information about them. Also allows you to toggle light and dark modes",
-      mobile: "/assets/testMob.jpeg",
-      desktop: "/assets/testTab.jpeg",
-      tablet: "/assets/testDex.jpeg",
+        "An app that displays all the countries in the world and important information about them. Also allows you to toggle light and dark modes.",
+      mobile: "/assets/restM.jpeg",
+      desktop: "/assets/restT.jpeg",
+      tablet: "/assets/restD.jpeg",
       github: "https://github.com/Davetech101/rest-countries",
-      link: "",
+      link: "https://glistening-toffee-6251d6.netlify.app/",
     },
     {
       id: "3",
@@ -48,9 +48,9 @@ const Projects = () => {
       tech: "React, Javascript, Firebase",
       about:
         "A real estate application where users can register to find and showcase houses for sale or rent.",
-      mobile: "/assets/testMob.jpeg",
-      desktop: "/assets/testTab.jpeg",
-      tablet: "/assets/testDex.jpeg",
+      mobile: "/assets/houseM.jpeg",
+      desktop: "/assets/houseT.jpeg",
+      tablet: "/assets/houseD.jpeg ",
       github: "https://github.com/Davetech101/housing-market",
       link: "https://tangerine-starburst-4dce6f.netlify.app/",
     },
@@ -60,11 +60,11 @@ const Projects = () => {
       tech: "React, Javascript",
       about:
         "Keep track of your daily tasks with this advanced Todo app which can filter your completed and active todos, you can also toggle light and dark modes.",
-      mobile: "/assets/testMob.jpeg",
-      desktop: "/assets/testTab.jpeg",
-      tablet: "/assets/testDex.jpeg",
+      mobile: "/assets/todoM.jpeg",
+      desktop: "/assets/todoT.jpeg",
+      tablet: "/assets/todoD.jpeg",
       github: "https://github.com/Davetech101/todoApp",
-      link: "",
+      link: "https://lustrous-scone-ac359e.netlify.app/",
     },
   ];
   return (
@@ -120,16 +120,21 @@ const Projects = () => {
               <p>{project.about}</p>
 
               <div className="btns">
-                <Link href={project.github}>View Code</Link>
-                <Link href={project.link}> View Website</Link>
+                <Link href={project.github} target="_blank">
+                  View Code
+                </Link>
+                <Link href={project.link} target="_blank">
+                  {" "}
+                  View Website
+                </Link>
               </div>
             </div>
           </div>
         ))}
       </div>
 
-      <GiStaticWaves className="wave"/>
-      <GiSandstorm className="storm"/>
+      <GiStaticWaves className="wave" />
+      <GiSandstorm className="storm" />
     </StyledProjects>
   );
 };
--- styles/styledcomponents/StyledProjects.jsx
+++ styles/styledcomponents/StyledProjects.jsx
@@ -276,7 +276,7 @@ const StyledProjects = styled.div`
     }
 
     img:hover {
-      transform: scale(.6);
+      transform: scale(.7);
       outline-offset: 1rem;
     }
 
